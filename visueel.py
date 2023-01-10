83da7eb2f1d18ef5f090047de80992f687420484
realysi <116087413+realysi@users.noreply.github.com>
2023-01-10 21:33:32 +0100

    Update visueel.py
    Leest nu ook de data in van de batterijen en de code is wat overzichtelijker gemaakt.
    Toekomstige problemen die we waarschijnlijk gaan tegenkomen: LIJNEN TEKENEN.

--- visueel.py
+++ visueel.py
@@ -2,38 +2,67 @@ import csv
 import numpy
 from matplotlib import pyplot
 
+
 """
 Functie om de data van de csv files in te lezen
+Toekomstige problemen: Lijnen tekenen (vragen begeleider of matplotlib hier wel ideaal voor is)
+Note! Heb veel enters in de code om het zo overzichtelijk mogelijk te houden voor de rest
 """
+
+pyplot.xlim(-0.2,51)   #limiet x-as
+pyplot.ylim(-0.2,51)   #limiet y-as
+
+pyplot.grid(which='major')  #toont major grid
+pyplot.grid(which='minor')  #toont minor grid
+
+#Data van huizen
 with open('/Users/yanickidsinga/Documents/GitHub/CGY-SmartGrid/Huizen&Batterijen/district_1/district-1_houses.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    pyplot.xlim(0,50)
-    pyplot.ylim(0,50)
-    gridlines = []
-    for i in range(50):
-        if i % 1 == 0 and i % 10 != 0:
-            gridlines.append(i)
-    print(gridlines)
-    pyplot.grid(which='major')
-    pyplot.grid(which='minor')
-    x = []
-    y = []
+
+
     line_count = 0
     for row in csv_reader:
-        if line_count != 0:
-            x.append(row[0])
-            y.append(row[1])
-            pyplot.plot(int(x[-1]), int(y[-1]), marker="p", color="black")
+        if line_count != 0: #skip eerste line 
+            x = row[0]
+            y = row[1]
+            pyplot.plot(int(x), int(y), marker="p", color="black")
         line_count += 1
-    pyplot.minorticks_on()
-    for i in gridlines:
-        pyplot.axhline(i, linestyle="-", color="black", linewidth=0.5)
-        pyplot.axvline(i, linestyle="-", color="black", linewidth=0.5)
-    pyplot.show()
+
+#Data van batterijen
+with open('/Users/yanickidsinga/Documents/GitHub/CGY-SmartGrid/Huizen&Batterijen/district_1/district-1_batteries.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count != 0: #skip eerste line 
+            x1 = row[0]
+            y1 = row[1]
+            pyplot.plot(int(x1), int(y1), marker="P", color="red")
+        line_count += 1
+
+axes_nummers = []
+for i in range(51):
+    if i % 5 == 0:
+        axes_nummers.append(i)
+        
+for i in axes_nummers:
+    pyplot.axhline(i, linestyle="-", color="black", linewidth=0.5)
+    pyplot.axvline(i, linestyle="-", color="black", linewidth=0.5)
+
+pyplot.xticks(ticks=axes_nummers, labels=axes_nummers) 
+pyplot.yticks(ticks=axes_nummers, labels=axes_nummers)
+
+#tonen grid 
+pyplot.minorticks_on() #toon minor gridlines (kan ticks nog uitschakelen -> zie links)
+pyplot.tight_layout()
+pyplot.show()
+
+
+
 
 """
 Links:
 https://www.tutorialspoint.com/how-can-i-plot-a-single-point-in-matplotlib-python
 https://www.pythoncharts.com/matplotlib/customizing-grid-matplotlib/
 https://stackoverflow.com/questions/14608483/how-to-add-a-grid-line-at-a-specific-location-in-matplotlib-plot
+
 """
