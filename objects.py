c77deb74b14c804144c388a21c47724377a42862
realysi <116087413+realysi@users.noreply.github.com>
2023-01-11 12:28:35 +0100

    House + battery classes

---
+++ objects.py
@@ -0,0 +1,11 @@
+class House:
+    def __init__(self, x, y, max_output):
+        self.x = x
+        self.y = y
+        self.max_output = max_output
+
+class Battery:
+    def __init__(self, x, y, capacity):
+        self.x = x
+        self.y = y
+        self.capacity = capacity
