3834252ecfe213ad1955dd8ff3822c077ab999af
Gabriel Veder <gabriel.veder@gmail.com>
2023-01-27 17:12:09 +0100

    Update
    Cabel class has to be rewritten or recreated so that houses can also connect to one another. A function should exist which takes four arguments x_start y_start, x_end and y_end. This data should be enough to create a cable between two points.

--- code/algorithms/cluster_algorithm.py
+++ code/algorithms/cluster_algorithm.py
@@ -13,6 +13,7 @@ def calc_middle(cluster, houses) -> Tuple[int, int]:
     Returns a tuple containing the coordinates which correspond to the middle of a given cluster.
     Does this by dividing the total x and y coordinates by the amount of houses in a cluster.
     """
+    # Keeps track of how many houses there are in a cluster
     cluster_size = 0
     x = 0
     y = 0
@@ -27,7 +28,8 @@ def calc_middle(cluster, houses) -> Tuple[int, int]:
 
 def closest_battery_to_cluster(middle: Tuple[int, int], batteries) -> Battery:
     """
-    Returns the battery object which is closest to the centre of a given cluster.
+    Returns the battery id of the battery
+    which is closest to the centre of a given cluster.
     """
     # Distance between center of cluster and battery cannot exceed 100 
     smallest_distance = 100
@@ -43,16 +45,12 @@ def closest_battery_to_cluster(middle: Tuple[int, int], batteries) -> Battery:
     return closest_battery_id
 
 
-def closest_house_to_battery(cluster, houses, batteries) -> House:
-    """
-    Returns house from cluster which is closest to the nearest battery.
-    """
-    for house_id in houses:
-
-
 def cluster_algorithm(houses, batteries):
     clusters = make_clusters(houses)
     for cluster in clusters:
         middle = calc_middle(cluster, houses)
         closest_battery = closest_battery_to_cluster(middle, batteries)
+        # The first house in the cluster will always be the closest to the battery
+        closest_house = cluster[0]
+        print(cluster)
     pass
