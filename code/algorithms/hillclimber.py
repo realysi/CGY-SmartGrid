f229951a86895142074990f351ffda31082a9983
Christaki313 <christos-aristotelis@hotmail.com>
2023-02-01 18:10:33 +0100

    copied hillclimber from Goede-versie

--- code/algorithms/hillclimber.py
+++ code/algorithms/hillclimber.py
@@ -1,13 +1,19 @@
 """
-Name algorithm: Hillclimber
+Name algorithm: Restart hill climber
 by: Yanick
+Description:
+This is a restart hill climber algorithm. It starts with a solution from the random algorithm and tries to improve
+the given dataset by trying to switch 2 houses of 2 different batteries with eachother. It checks if in n-tries none of the
+batteries capacities are exceeded due to the switch and wether the switch results in lower costs. If these 3 
+requirements are met, then the switch is made permanent. 
 """
+import csv
 from matplotlib import pyplot as plt
 from .random_algorithm import random_algorithm
 from code.classes.data import Data
-from code.classes.cable import Cable
 from code.classes.house import House
 from code.classes.battery import Battery
+#from code.visualisation.hillclimber_views import sketch
 import random
 import copy
 
@@ -104,6 +110,7 @@ def valid_switch(data: Data) -> bool:
             return False
     return True
 
+
 def better_score_after_switch(data_before_switch: Data, data_after_switch: Data) -> bool:
     """
     Returns a bool.
@@ -117,146 +124,112 @@ def better_score_after_switch(data_before_switch: Data, data_after_switch: Data)
     else:
         return False
 
-def switch_max_outputs(data: Data, house_one: House, house_two: House):
-    """
-    Returns nothing
-    Switches the max_outputs of the two houses on the two batteries.
-    """
-    #removes max_output from the capacity of the batteries
-    data.batteries[house_one.to_battery].capacity += house_one.max_output  #remove output from current house from the capacity
-    data.batteries[house_two.to_battery].capacity += house_two.max_output  #"same as above"
 
-    #adds max_output from the houses to the capacity of the batteries
-    data.batteries[house_one.to_battery].capacity -= house_two.max_output  #switch the outputs from the houses to the capacity of the batteries
-    data.batteries[house_two.to_battery].capacity -= house_one.max_output  #"same as above"
-
-def switch_to_houses(data: Data, house_one: House, house_two: House):
+def clear_segments(data: Data, choice_houses:list[House]):
     """
-    Returns nothing
-    Switches the house_id's of the two houses on the to_houses of the two batteries.
+    Clears the segments of the houses that will be switched from batteries
     """
-    #remove house from to_house of battery
-    data.batteries[house_one.to_battery].to_houses.remove(house_one.id)
-    data.batteries[house_two.to_battery].to_houses.remove(house_two.id)
+    for i in choice_houses:
+        data.cables[i.id].segments.clear()
 
-    #add house to to_house of battery
-    data.batteries[house_one.to_battery].to_houses.append(house_two.id)
-    data.batteries[house_two.to_battery].to_houses.append(house_one.id)
 
-def runs(tries):
-    if tries < 10:
+def runs(tries: int, limit) -> bool:
+    """
+    returns True if amount of tries is under the given limit.
+    """
+    if tries < limit:
         return True
     else: 
         return False
 
-def sketch(results: dict):
-    scores = []
-    depth = []
-    for hillclimber in results:
-        scores.append(results[hillclimber].cost)
-        depth.append(results[hillclimber].depth)
 
-    best_score = min(scores)
-
-    for i in results:
-        if results[i].cost == best_score:
-            data_best_score = results[i]
-
-    average_depth = sum(scores) / len(scores)
-
-
-#makes it well enough to a depth of 60 in an okay time (10 min)
-#further than 100 steps can take up to an hour and a half
-
-        
-    plt.hist(scores)
-    plt.show()
-    plt.hist(depth)
-    plt.show()
-
-def switch(data: Data) -> Data:
-    """
-    Returns data object 
-    Edits data object and if valid_switch() and beter_score_after_switch() return true
-    it returns the edited data object, otherwise the old object.
-    """
-    tries = 0
+def switch(data: Data, amount_of_houses, limit):
     while True:
-        if runs(tries):
+        if runs(data.depth, limit):
+            data.depth += 1
             copy_data: Data = copy.deepcopy(data)
-            house_one: House = select_house_one(copy_data)
-            house_two: House = select_house_two(copy_data, house_one.id)
 
-            switch_max_outputs(copy_data, house_one, house_two)
-            switch_to_houses(copy_data, house_one, house_two)
+            houses = select_houses(copy_data, amount_of_houses)
+            #switch_houses(data, houses)
+            switch_max(copy_data, houses)
 
-            copy_house_one_battery: int = copy.deepcopy(house_one.to_battery) #make a deepcopy of house_one battery as buffer
-            house_one.to_battery = house_two.to_battery #switch batteries of house_one and house_two
-            house_two.to_battery = copy_house_one_battery
+            switch_bat(copy_data, houses)
 
-            copy_data.cables[house_one.id].segments.clear()
-            copy_data.cables[house_two.id].segments.clear()
-
-            copy_data.add_cables() #only should have to do this for two switched houses
+            clear_segments(copy_data, houses)
+            copy_data.add_cables()
 
             if valid_switch(copy_data):
                 if better_score_after_switch(data, copy_data):
-                    #print(data.cost_with_overlay(), copy_data.cost_with_overlay())
-                    #print(copy_data.cables[house_one.id].segments, copy_data.cables[house_two.id].segments)
-                    #print(tries)
+                    #print(f"cost: {copy_data.cost}\t| depth : {data.depth}")
                     return copy_data
+                    
                 else:
-                    tries += 1
+                    #print(f"cost: {data.cost}\t| depth : {data.depth}")
                     continue         
             else:
-                tries += 1
+                #print(f"cost: {data.cost}\t| depth : {data.depth}")
                 continue
-
         else:
             return data
             break
 
-        #fout zit hem erin dat als de huizen en batterijen worden geswitched, de kabels en dus segmenten dat nog niet doen --> nog oplossen
+    pass
 
-def hillclimber(houses, batteries) -> Data:
+
+def random_solution(houses: dict[int, House], batteries: dict[int, Battery]) -> Data:
+    """
+    Returns data object which has been solved by the random algorithm and alreay has the segments and costs calculated.
+    """
     data: Data = random_algorithm(houses, batteries)
     data.add_cables()
-    data.cost_with_overlay() #calculates cost of grid
-    for i in range(20):
-        copy_data = data
-        data = switch(data)
-        if copy_data == data:
-            print("JA")
-        
-        print(f"\n{i}\n")
+    data.cost_with_overlay()
 
     return data
 
-def restart_hillclimber(houses, batteries):
-    results = {}
-    total_hillclimbers = 1
-    while total_hillclimbers < 6:
-        data: Data = random_algorithm(houses, batteries)
-        data.add_cables()
-        data.cost_with_overlay()
-        for i in range(100):
-            #print(f"TRY: {total_hillclimbers}")
-            copy_data = data
-            data = switch(data)
-            data.depth += 1
-            if copy_data == data:
-                print(f"cost: {data.cost} | depth : {data.depth}")
-                #print("TRY TERMINDATED:")
-                results[total_hillclimbers] = data
-                total_hillclimbers += 1
-                break
-            else:
-                continue
 
+def restart_hillclimber(houses, batteries, amount_of_houses) -> dict[int, Data]:
+    """"
+    Returns dictionary that contains data object of all hill climber runs (key = number of run, value = data object).
+    Hill climber algorithm that restarts for x amount of times if after n-runs no beter solution has been found.
+    """
+    results = {}
+    total_hillclimbers: int = 0
+    limit = 100
+    while total_hillclimbers < 1:
+        data: Data = random_solution(houses, batteries)
+
+        while data.depth < limit:
+            data = switch(data, amount_of_houses, limit)
+            
+        data.algorithm_used = "hill climber"
+        data.base = "random"
+
+        print(f"run: {total_hillclimbers}\t| cost: {data.cost}\t| depth : {data.depth}")
+        results[total_hillclimbers + 1] = data
+        total_hillclimbers += 1
+
+            
+    save_data(results)
     print(results)
-    sketch(results)
+    #sketch(results)
     return results
 
+def save_data(dictionary: dict[int, Data]):
+    fields = ["Run", "Depth", "Cost", "Algorithm", "Base"]
+    filename = "hillclimber.csv"
+
+    with open(filename, "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+
+        csv_writer.writerow(fields)
+        for i in dictionary:
+            run = i
+            depth = dictionary[i].depth
+            cost = dictionary[i].cost
+            algo = dictionary[i].algorithm_used
+            algo_base = dictionary[i].base
+            row = [run, depth, cost, algo, algo_base]
+            csv_writer.writerows([row])
 
     
 
